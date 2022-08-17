12adc0fc5901ae7243812ad89363bd9bd4f59018
lucas <lucas.dantas.nogueira@gmail.com>
2022-08-17 11:21:42 -0300

    editando e criando rotas

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,21 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Route::name('products.')->group(function () {
+    Route::put('products/{id}', 'ProductController@update')->name('update');
+    Route::get('products/{id}/edit', 'ProductController@edit')->name('edit');
+    Route::get('products/create', 'ProductController@create')->name('create');
+    Route::get('products/{id}', 'ProductController@show')->name('show');
+    Route::get('products', 'ProductController@index')->name('index');
+    Route::post('products', 'ProductController@store')->name('store');
+});
+
+
+
+//rota de login caso entre do middleware auth
+Route::get('/login', function () {
+    return 'Login';
+})->name('login');
+
 
 Route::get('/', function () {
     return view('welcome');
