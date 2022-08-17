e30cbbac3caab1723a32f550f0a615ff65a86b0e
lucas <lucas.dantas.nogueira@gmail.com>
2022-08-17 11:22:08 -0300

    diretorio criado a partir do artisan

---
+++ app/Http/Controllers/Admin/TesteController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class TesteController extends Controller
+{
+    public function teste()
+    {
+        return 'Teste Controller';
+    }
+}
