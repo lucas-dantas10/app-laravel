18dd2014c5267b7ea5edaaa7baa22e0296d51605
lucas <lucas.dantas.nogueira@gmail.com>
2022-08-22 14:47:43 -0300

    adicionando rotas

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,18 @@
 <?php
 
-Route::name('products.')->group(function () {
+// CRUD nas rotas simplificado
+Route::resource('products', 'ProductController'); //->middleware('auth');
+
+// CRUD nas rotas mais verboso
+/* Route::name('products.')->group(function () {
+    Route::delete('products/{id}', 'ProductController@destroy')->name('destroy');
     Route::put('products/{id}', 'ProductController@update')->name('update');
     Route::get('products/{id}/edit', 'ProductController@edit')->name('edit');
     Route::get('products/create', 'ProductController@create')->name('create');
     Route::get('products/{id}', 'ProductController@show')->name('show');
     Route::get('products', 'ProductController@index')->name('index');
     Route::post('products', 'ProductController@store')->name('store');
-});
+}); */
 
 
 
